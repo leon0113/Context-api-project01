5fa79f6a0784f96e8ceaf3a729f4f71bc8ca9178
Leon <tahjib.leon399@gmail.com>
2023-10-25 09:22:22 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import './App.css'
+import Divider from './components/Divider'
+import Login from './components/Login'
+import Profile from './components/Profile'
 import UserContextProvider from './context/UserContextProvider'
 
 function App() {
@@ -6,6 +9,9 @@ function App() {
   return (
     <UserContextProvider>
       <h1>React Context API</h1>
+      <Login />
+      <Divider />
+      <Profile />
     </UserContextProvider>
   )
 }
---
+++ src/components/Divider.jsx
@@ -0,0 +1,8 @@
+
+function Divider() {
+    return (
+        <div>------------------------------------------</div>
+    )
+}
+
+export default Divider
